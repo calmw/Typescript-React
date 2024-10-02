a59f1669ba1563d536b26cd5a83d506dc42c13a4
calmw <calm.wang@hotmail.com>
2024-10-02 15:42:20 +0800

    webpack

---
+++ webpack-ts/dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+eval("// TODO: define variable\r\n// let s: String = \"Hello, this is Webpack app (by TypeScript).\";\r\n//\r\n// // TODO: print\r\n// console.log(s);\r\n\r\n// document operation\r\ndocument.getElementsByTagName('p')[0].innerText = \"Hello, this is Webpack app (by TypeScript).\";\n\n//# sourceURL=webpack://webpack-ts/./src/index.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ })()
+;
